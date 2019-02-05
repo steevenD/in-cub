73595da431a603f1c6a0cca0fad73607c281a78d
Marion OLIVER <marion.oliver@outlook.fr>
2019-02-05 16:59:29 +0100

    startup & consultant model

--- Frontend/src/app/consultant/consultant.model.ts
+++ Frontend/src/app/consultant/consultant.model.ts
@@ -1,6 +1,19 @@
 
 export class Consultant {
   id: number;
+  lastname: string;
+  firstname: string;
+  description: string;
 
-  constructor() { }
+  constructor(
+    id: number,
+    lastname: string,
+    firstname: string,
+    description: string
+  ) {
+    this.id = id;
+    this.lastname = lastname;
+    this.firstname = firstname;
+    this.description = description;
+   }
 }
--- Frontend/src/app/startup/startup.model.ts
+++ Frontend/src/app/startup/startup.model.ts
@@ -1,6 +1,32 @@
+import { Consultant } from './../consultant/consultant.model';
 
 export class Startup {
   id: number;
+  name: string;
+  businessLine: string;
+  legalRepresentativeName: string;
+  cofounderNumber: number;
+  description: string;
+  address: string;
+  consultant: Consultant;
 
-  constructor() { }
+  constructor(
+    id: number,
+    name: string,
+    businessLine: string,
+    legalRepresentativeName: string,
+    cofounderNumber: number,
+    description: string,
+    address: string,
+    consultant: Consultant
+  ) {
+    this.id = id;
+    this.name = name;
+    this.businessLine = businessLine;
+    this.legalRepresentativeName = legalRepresentativeName;
+    this.cofounderNumber = cofounderNumber;
+    this.description = description;
+    this.address = address;
+    this.consultant = consultant;
+   }
 }
